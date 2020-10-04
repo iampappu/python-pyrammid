df9b5cad6a8b235971212c8060aea4e9631bbd7e
Arindam <arindamghosh854@gmail.com>
2020-10-04 12:16:59 +0530

    Added User Input

--- pattern_pyramid-i.py
+++ pattern_pyramid-i.py
@@ -1,5 +1,6 @@
-for i in range(1,6):
-    for j in range(i,5):
+num=int(input("Enter Line Number : "))
+for i in range(1,num+1):
+    for j in range(i,num):
         print(" ",end="")
     for j in range(i):
         print("*",end="")
